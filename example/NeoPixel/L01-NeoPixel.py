19bea02e648668e5c3cbdc600a5377afe62efba3
Teeraphat Kullanankanjana <ku.teeraphat_st@tni.ac.th>
2022-05-23 11:25:48 +0700

    Setting R,G,B Value to NeoPixel
    Setting R,G,B Value to NeoPixel

--- example/NeoPixel/L01-NeoPixel.py
+++ example/NeoPixel/L01-NeoPixel.py
@@ -3,9 +3,9 @@ from neopixel import NeoPixel
 from machine import Pin
 
 # ---Pin Setup---
-np_Pin = NeoPixel(Pin(23,Pin.OUT),2) # 2 is Number of NeoPixel
+np_Pin = NeoPixel(Pin(23,Pin.OUT),2) # 2 is number of NeoPixel
 
 # --- Main Program ---
-np_Pin[0] = (10,0,0) # Set RGB 1 to 10% red 
-np_Pin[1] = (0,0,10) # Set RGB 2 to 10% blue
-np_Pin.write()       # Light up the Neo Pixel 
+np_Pin[0] = (10,0,0) # set RGB 1 to 10% red 
+np_Pin[1] = (0,0,10) # set RGB 2 to 10% blue
+np_Pin.write()       # write the setting rgb value to NeoPixel
