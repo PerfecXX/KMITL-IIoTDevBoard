ba7c0af5eb052fd4c19c10cca34c21381c6ba09b
Teeraphat Kullanankanjana <watit2013@gmail.com>
2023-10-03 11:32:26 +0700

    Connect to WiFi

--- example/Network/L02-ConnectWiFi.py
+++ example/Network/L02-ConnectWiFi.py
@@ -2,18 +2,22 @@
 from network import WLAN,STA_IF
 
 # Network Credential
-ssid = ''
-password = ''
+ssid = 'replace_with_your_ssid'
+password = 'replace_with_your_password'
 
 # Network Interface 
-wlan = WLAN(STA_IF)
-wlan.active(True)
-
-# Start Connection
-print('Connecting...')
-wlan.connect(ssid,password)
-while not wlan.isconnected():
-    pass
+sta_if = WLAN(STA_IF)
+# Activate the station mode
+sta_if.active(True)
+# Check if the devices are already connected or not.
+# If it is not connected, then start the connection.
+if not sta_if.isconnected():
+   print("Connecting to wifi: ", ssid)
+   sta_if.connect(ssid, password)
+   # Wait until your devices are connected.
+   while not sta_if.isconnected():
+       pass
+print("Connection successful")
 
 # Connection Config
 print(wlan.ifconfig())
